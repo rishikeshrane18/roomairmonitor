43e5ea0a479e62089da5b2d0393b1c19c4c400e6
dakshp07 <dpaleria@gmail.com>
2021-06-06 11:23:52 +0530

    Updated API Calls

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,9 +6,8 @@ void main() {
   runApp(MaterialApp(
     theme: new ThemeData(
         colorScheme: ColorScheme.light(
-          background: Colors.black,
-        )
-    ),
+      background: Colors.black,
+    )),
     home: new HomePage(),
   ));
 }
@@ -20,11 +18,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   Map pokeData;
 
   fetchPokemonData() async {
-    http.Response response = await http.get(Uri.parse("https://api.thingspeak.com/channels/1408525/feeds.json?api_key=Z39TIBA5I7P4372H&results=2"));
+    http.Response response = await http.get(Uri.parse(
+        "https://api.thingspeak.com/channels/1408525/feeds.json?api_key=Z39TIBA5I7P4372H&results=2"));
     setState(() {
       pokeData = json.decode(response.body);
       print('hello: $pokeData');
@@ -38,40 +36,38 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: Center(child: new Text("POKEMONE APP")),
         backgroundColor: Colors.lightBlue,
-
       ),
       body: new Container(
         child: new Center(
-          child: pokeData == null ? new CircularProgressIndicator() : new ListView.builder(
-            itemCount : pokeData['pokemon'].length,
-            itemBuilder: (BuildContext context, int index) {
-              return Center(
-                child: new Card(
-                  child: Center(
-                    child: new Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        new Text("NAME : " + pokeData["channel"]['name']),
-                        new Text("Id : " + pokeData["feed"][index]['field1']),
-                       // new Image.network( pokeData['pokemon'][index]['img']),
-
-                      ],
-                    ),
-                  ),
+          child: pokeData == null
+              ? new CircularProgressIndicator()
+              : new ListView.builder(
+                  itemCount: pokeData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Center(
+                      child: new Card(
+                        child: Center(
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              new Text("NAME : " + pokeData["channel"]['name']),
+                              new Text("Id : " +
+                                  pokeData["feeds"][index]['field1']
+                                      .toString()),
+                              // new Image.network( pokeData['pokemon'][index]['img']),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-
-
-          ),
-
         ),
       ),
     );
